a2d03389ea5b2720d86aa24c00686a0cc5a92196
IceAmanegi <iceamanegi@gmail.com>
2025-08-17 15:24:06 +0900

    firstcommit

--- src/app/component/Drawermenu.tsx
+++ src/app/component/Drawermenu.tsx
@@ -21,7 +21,7 @@ export default function Drawermenu() {
   return (
     <div className="App">
       <button className="circle-button" onClick={() => setIsOpen(true)}>
-        {/* <img src={menuIcon} alt="メニュー" />  a*/}
+        {/*  <img src={menuIcon}  alt="メニュー" />  */}
       </button>
       
       <div
